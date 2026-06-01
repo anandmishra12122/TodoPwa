c877d5cda3f9e4620d5767aca17f37fa7e4ebcba
anandmishra12122 <anandmishra12122@gmail.com>
2026-06-02 04:22:36 +0530

    Add activate event and skipWaiting to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 self.addEventListener("install", (event) => {
-  console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
